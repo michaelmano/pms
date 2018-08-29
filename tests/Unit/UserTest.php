495bfbba4cfa31abd683a425d914b7ac776c5809
Michael Mano <michael.mano26@gmail.com>
2018-08-29 14:22:36 +1000

    Refactor Tests

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\User;
+use App\Role;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Database\QueryException;
@@ -11,7 +13,7 @@ class UserTest extends TestCase
     /** @test */
     public function a_user_has_a_first_and_last_name()
     {
-        $user = factory(\App\User::class)->create();
+        $user = factory(User::class)->create();
 
         $this->assertNotEmpty($user->first_name);
         $this->assertNotEmpty($user->last_name);
@@ -20,16 +22,16 @@ class UserTest extends TestCase
     /** @test */
     public function a_user_starts_with_no_roles()
     {
-        $user = factory(\App\User::class)->create();
+        $user = factory(User::class)->create();
         $this->assertEmpty($user->roles);
     }
 
     /** @test */
     public function a_user_can_have_many_roles()
     {
-        $user = factory(\App\User::class)->create();
-        $role_one = factory(\App\Role::class)->create();
-        $role_two = factory(\App\Role::class)->create();
+        $user = factory(User::class)->create();
+        $role_one = factory(Role::class)->create();
+        $role_two = factory(Role::class)->create();
 
         $user->roles()->attach($role_one);
 
@@ -44,8 +46,8 @@ class UserTest extends TestCase
     public function a_user_can_have_many_roles_but_they_have_to_be_unique()
     {
 
-        $user = factory(\App\User::class)->create();
-        $role = factory(\App\Role::class)->create();
+        $user = factory(User::class)->create();
+        $role = factory(Role::class)->create();
 
         try {
             $user->roles()->attach($role);
