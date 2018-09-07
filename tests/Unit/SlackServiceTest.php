1968a17b7f77a4266c4aaf56c367b6fe9a235695
Michael Mano <michael.mano26@gmailc.com>
2018-09-07 10:33:20 +1000

    Refactor the Slack test to save time

--- tests/Unit/SlackServiceTest.php
+++ tests/Unit/SlackServiceTest.php
@@ -12,11 +12,6 @@ class SlackServiceTest extends TestCase
     public function the_service_configuration_has_slack_details()
     {
         $this->assertNotEmpty(config('services.slack'));
-    }
-
-    /** @test */
-    public function the_slack_config_has_a_token()
-    {
         $this->assertNotEmpty(config('services.slack.token'));
     }
 
@@ -29,22 +24,19 @@ class SlackServiceTest extends TestCase
     /** @test */
     public function the_slack_service_can_find_a_user_if_they_exist()
     {
-        // Gets slack data
-        $slack_data = json_decode((new SlackService())->getUsers());
-
+        $slack_api = new SlackService();
         // Finds the first user with an email
-        $first_slack_user = collect($slack_data->members)
-            ->filter(function ($member) {
-                return isset($member->profile->email);
-            })->first();
+        $first_slack_user = collect(
+            json_decode($slack_api->getUsers())->members
+        )->filter(function ($member) {
+            return isset($member->profile->email);
+        })->first();
 
         // Creates a user with that email
         $user = factory(User::class)->create([
             'email' => $first_slack_user->profile->email,
         ]);
 
-        $api = new SlackService();
-
-        $this->assertNotEmpty($api->find($user));
+        $this->assertNotEmpty($slack_api->find($user));
     }
 }
