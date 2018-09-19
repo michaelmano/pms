ed86f636deb9df9468300ec9172f810742f3a516
Michael Mano <michael.mano26@gmailc.com>
2018-09-19 16:22:26 +1000

    Add Users Away table

--- database/migrations/2018_09_19_062020_create_users_away_table.php
+++ database/migrations/2018_09_19_062020_create_users_away_table.php
@@ -4,34 +4,35 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectsTable extends Migration
+class CreateUsersAwayTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('users_away', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('client_id')
+            $table->integer('user_id')
                 ->unsigned();
-            $table->foreign('client_id')
+            $table->foreign('user_id')
                 ->references('id')
-                ->on('clients')
+                ->on('users')
                 ->onDelete('cascade');
-
-            $table->string('job_code');
-            $table->string('title');
-            $table->text('description');
+            $table->dateTime('returning');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('users_away');
     }
 }
