69cf6a80e3ae1968c6b29e157bf47e92d8859c7f
Michael Mano <michael.mano26@gmailc.com>
2018-09-20 12:27:57 +1000

    Add right sidebar

--- resources/assets/js/components/Header.vue
+++ resources/assets/js/components/Header.vue
@@ -3,16 +3,23 @@
     <a href="/dashboard" title="Durian Project Management System">
       <vue-icon name="durian" fill="alpha"/>
     </a>
-    <a href="/dashboard" title="Durian Project Management System">
+    <button @click="toggleSidebar">
       <vue-icon name="fa-solid-cog" fill="alpha"/>
-    </a>
+    </button>
+    <div class="Header__sidebar" v-show="showSidebar">
+      <ul>
+        <li><button aria-label="Logout" class="Header__sidebar-link" @click="logout">Logout</button></li>
+      </ul>
+    </div>
   </header>
 </template>
 <script>
 export default {
   props: ['user'],
   data() {
-    return {};
+    return {
+      showSidebar: false,
+    };
   },
   computed: {},
   watch: {},
@@ -21,6 +28,10 @@ export default {
   mounted() {
   },
   methods: {
+    toggleSidebar() {
+      console.log('triggered');
+      this.showSidebar = !this.showSidebar;
+    },
     logout() {
       return axios
         .post('/auth/logout')
